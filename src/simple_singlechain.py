8b53c4afab670ce25236efb70f643ad5ba2f0595
fancaoErik <ckv176@sbinlabcomp01fl.unicph.domain>
2024-03-12 13:56:51 +0100

    submit

---
+++ src/simple_singlechain.py
@@ -0,0 +1,153 @@
+"""
+This script is not designed for any HPC platform. It can be used for a quick simulation for your protein;
+Just modify the basic settings in Block 2, you can get a coarse-grained trajectory with CALVADOS3;
+"""
+########################################################################################################################
+#                                   1. import modules                                                                   #
+########################################################################################################################
+from utils import *
+from rawdata import *
+import os
+from protein_repo import get_ssdomains
+
+########################################################################################################################
+#                2. you only need to change parameters below to customize your protein simulation                      #
+########################################################################################################################
+cwd = "/projects/prism/people/ckv176/_2024_Cao_CALVADOSCOM/src"  # absolute path to "simple_singlechain.py", this script
+
+# add IDPs or MDPs;
+record = "GS48"  # protein name
+temp = 293  # unit: K
+pH = 7.5
+ionic = 0.15  # unit: M
+fasta = list("SKGEELFTGVVPILVELDGDVNGHKFSVSGEGEGDATYGKLTLKFICTTGKLPVPWPTLVTTLSWGVQCFARYPDHMKQHDFFKSAMPEGYVQERTIFFKDDGNYKTRAEVKFEGDTLVNRIELKGIDFKEDGNILGHKLEYNYFSDNVYITADKQKNGIKANFKIRHNIEDGGVQLADHYQQNTPIGDGPVLLPDNHYLSTQSKLSKDPNEKRDHMVLLEFVTAAGITLGMDELYKEGLSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSGSKLMVSKGEEDNMASLPATHELHIFGSINGVDFDMVGQGTGNPNDGYEELNLKSTKGDLQFSPWILVPHIGYGFHQYLPYPDGMSPFQAAMVDGSGYQVHRTMQFEDGASLTVNYRYTYEGSHIKGEAQVKGTGFPADGPVMTNSLTAADWCRSKKTYPNDKTIISTFKWSYTTGNGKRYRSTARTTYTFAKPMAANYLKNQPMYVFRKTELKHSKTELNFKEWQKAFTD")
+replicas = 20  # nums of simulation replica for your protein
+
+# is it a IDP or MDP?
+isIDP = False
+if not isIDP:
+    domain_boundaries = {record: [[1,226], [352,566]]}  # define domain boundaries; for more information, please check step3 in "Run single-chain simulations with CALVADOS 3" from https://github.com/KULL-Centre/_2024_Cao_CALVADOSCOM/blob/main/README.md
+    path2pdb = '/home/ckv176/_2024_Cao_CALVADOSCOM/src/extract_relax/GS48_rank0_relax.pdb'  # absolute path to atomistic structure
+
+# customize your desired simulation time, unit: ns;
+customized_simulation_time = 5
+if customized_simulation_time == None:  # if None, script will use the default simulation duration depending on the IDR sequence length;
+    # you don't have to change this block
+    nframes = 200  # total number of frames to keep for each replica (exclude discarded frames)
+    discard_first_nframes = 10  # the first ${discard_first_nframes} will be discarded for each replica
+else:
+    # you have to modify this block if you don't set 'customized_simulation_time' to 'None'
+    interval = 0.01  #  time interval to save each frame, unit: ns
+
+########################################################################################################################
+#          3. no need to change parameters below unless you know exactly what you are doing                            #
+########################################################################################################################
+initial_type = "C3"  # which forcefield to use; C3 means CALVADOS3;
+CoarseGrained = "COM"  # COM: Center of mass; CA: Cα; CoarseGrained strategy;
+k_restraint = 700  # unit:KJ/(mol*nm^2); prior default value: 700; force constant of elastic network model;
+purpose = "CALVADOS3"  # CALVADOS3 means using CALVADOS3 to simulate proteins;
+dataset_replica = 1
+cutoff = 2.0  # 2.0 nm for production
+cycle = 0  # optimization cycles, 0-based, only for optimization
+gpu = False  # gpu acceleration
+eps_factor = 0.2
+gpu_id = 0
+Usecheckpoint = False
+slab = False
+
+########################################################################################################################
+#                                            4. submit simulations                                                     #
+########################################################################################################################
+dataset = f"{purpose}{CoarseGrained}_{cutoff}_{dataset_replica}"
+if not os.path.isdir(f"{cwd}/{dataset}"):
+    os.system(f"mkdir -p {cwd}/{dataset}")
+os.system(f"cp {cwd}/residues_pub.csv {cwd}/{dataset}")
+create_parameters(cwd, dataset, cycle, initial_type)
+# simulate
+name = record.split("@")[0]
+L = int(np.ceil((len(fasta) - 1) * 0.38 + 4))  # cubic box size setting, unit: nm
+fdomains = f'{cwd}/{dataset}/domain_simple.yaml'
+if not isIDP:
+    yaml.dump(domain_boundaries, open(fdomains, 'w'))
+    input_pae = ""  # decide_best_pae(cwd, name)
+    use_pdb = True
+    use_hnetwork = True
+    use_ssdomains = True
+    if customized_simulation_time == None:
+        domain_len = 0
+        for domain in get_ssdomains(name, fdomains, output=False):
+            domain_len += len(domain)
+        N_res = int(len(fasta) - domain_len)
+        N_save = 3000 if N_res < 100 else int(np.ceil(3e-4 * N_res ** 2) * 1000)  # interval
+        N_steps = (nframes + discard_first_nframes) * int(N_save)
+    else:
+        N_steps = int(customized_simulation_time * 1E5)
+        total_frames = int(customized_simulation_time / interval)
+        discard_first_nframes = int(total_frames * 0.05)
+        nframes = int(total_frames - discard_first_nframes)
+        N_save = int(interval * 1E5)
+else:
+    input_pae = None
+    path2pdb = ""
+    use_pdb = False
+    use_hnetwork = False
+    use_ssdomains = False
+    if customized_simulation_time == None:
+        N_save = 3000 if len(fasta) < 100 else int(np.ceil(3e-4 * len(fasta) ** 2) * 1000)  # interval
+        N_steps = (nframes + discard_first_nframes) * int(N_save)
+    else:
+        N_steps = int(customized_simulation_time * 1E5)
+        total_frames = int(customized_simulation_time / interval)
+        discard_first_nframes = int(total_frames * 0.05)
+        nframes = int(total_frames - discard_first_nframes)
+        N_save = int(interval * 1E5)
+
+if not os.path.isdir(f"{cwd}/{dataset}/{record}/{cycle}"):
+    os.system(f"mkdir -p {cwd}/{dataset}/{record}/{cycle}")
+
+replicas_list4MD = list(range(replicas))
+config_sim_filename = f'config_sim.yaml'
+config_sim_data = dict(cwd=cwd, name=name, dataset=dataset, temp=temp, ionic=ionic, cycle=cycle, pH=pH,
+   replicas_list4MD=replicas_list4MD, cutoff=cutoff, L=L, wfreq=int(N_save), slab=slab,
+   use_pdb=use_pdb, path2pdb=path2pdb, use_hnetwork=use_hnetwork, fdomains=fdomains,
+   use_ssdomains=use_ssdomains, input_pae=input_pae, k_restraint=k_restraint, record=record,
+   gpu_id=gpu_id%4, Threads=1, overwrite=True, N_res=len(fasta),
+   CoarseGrained=CoarseGrained, isIDP=isIDP, Usecheckpoint=Usecheckpoint, eps_factor=eps_factor,
+   initial_type=initial_type, seq=fasta, steps=N_steps, gpu=gpu, replicas=replicas,
+   discard_first_nframes=discard_first_nframes,validate=False, nframes=nframes)
+
+ray.init(num_cpus=len(replicas_list4MD), include_dashboard=False)
+for replica in replicas_list4MD:
+    config_sim_data["replica"] = replica
+    yaml.dump(config_sim_data, open(f"{cwd}/{dataset}/{record}/{cycle}/config_{replica}.yaml", 'w'))
+ray.get([simulate_simple.remote(yaml.safe_load(open(f"{cwd}/{dataset}/{record}/{cycle}/config_{replica}.yaml", 'r'))) for replica in replicas_list4MD])
+
+config_merge_data = dict(cwd=cwd, name=name, dataset=dataset, temp=temp, ionic=ionic,
+                         cycle=cycle, pH=pH, replicas_list4MD=replicas_list4MD, cutoff=cutoff, L=L,
+                         wfreq=int(N_save), slab=slab, use_pdb=use_pdb, path2pdb=path2pdb,
+                         use_hnetwork=use_hnetwork, fdomains=fdomains, use_ssdomains=use_ssdomains,
+                         input_pae=input_pae, k_restraint=k_restraint, record=record, gpu_id=gpu_id % 4, Threads=1,
+                         overwrite=True, N_res=len(fasta), CoarseGrained=CoarseGrained, isIDP=isIDP,
+                         Usecheckpoint=Usecheckpoint, eps_factor=eps_factor, initial_type=initial_type, seq=fasta,
+                         steps=N_steps, gpu=gpu, replicas=replicas, discard_first_nframes=discard_first_nframes,
+                         validate=False, nframes=nframes)
+centerDCD_simple(config_merge_data)
+# you will get a trajectory ${record}.dcd and a corresponding topology file ${record}.pdb after performing codes above;
+# the trajectory file ${record}.dcd contains all the trajectories from each replica;
+# trajectory file ${record}.dcd can be used for post analysis, like calculating Rg (see below);
+
+# calculate simulated Rg
+df = load_parameters(cwd, dataset, cycle, initial_type).set_index("three")
+t = md.load_dcd(f"{cwd}/{dataset}/{record}/{cycle}/{record}.dcd",
+                f"{cwd}/{dataset}/{record}/{cycle}/{record}.pdb")  # nm
+residues = [res.name for res in t.top.atoms]
+masses = df.loc[residues,'MW'].values
+masses[0] += 2
+masses[-1] += 16
+# calculate the center of mass
+cm = np.sum(t.xyz*masses[np.newaxis,:,np.newaxis],axis=1)/masses.sum()
+# calculate residue-cm distances
+si = np.linalg.norm(t.xyz - cm[:,np.newaxis,:],axis=2)
+# calculate rg
+rgarray = np.sqrt(np.sum(si**2*masses,axis=1)/masses.sum())
+np.save(f"{cwd}/{dataset}/{record}/{cycle}/Rg_traj.npy", rgarray)
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+from misc_tools import *
+from openmm import app
 import os.path
 import time
 from DEERPREdict.PRE import PREpredict
@@ -859,6 +861,228 @@ def takeWorkerports(cwd, num_of_jobs):
             workerports.remove(port)
         np.save(f"{cwd}/workerports.npy", workerports)
         return picked_workerports
+
+@ray.remote(num_cpus=1)  # specify num_cpus to make sure well-distributed jobs
+def simulate_simple(config):
+    """ Simulate openMM Calvados
+
+        * config is a dictionary """
+
+    # parse config
+    record = config["record"]
+    overwrite = config["overwrite"]
+    dataset = config["dataset"]
+    Usecheckpoint = config["Usecheckpoint"]
+    name, temp, ionic = config['name'], config['temp'], config['ionic']
+    cutoff, steps, wfreq = config['cutoff'], config['steps'], config['wfreq']
+    L = config['L']
+    eps_factor = config["eps_factor"]
+    pH = config["pH"]
+    isIDP = config['isIDP']
+    CoarseGrained = config['CoarseGrained']
+    gpu_id = config['gpu_id']
+    gpu = config['gpu']
+    seq = config['seq']
+    cycle = config["cycle"]
+    replica = config["replica"]
+    cwd = config['cwd']
+    os.system(f"uname -n>{cwd}/{dataset}/{record}/{cycle}/{replica}_uname.txt")
+    use_pdb, path2pdb = config['use_pdb'], config['path2pdb']
+    slab = config['slab']
+    Threads = config["Threads"]
+    fdomains = config['fdomains']
+    initial_type = config['initial_type']
+    print("use_pdb: ", use_pdb)
+    print("Threads: ", Threads)
+    center_of_mass = np.array([0,0,0])
+    if use_pdb:
+        use_hnetwork = config['use_hnetwork']
+        if use_hnetwork:
+            k_restraint = config['k_restraint']
+            use_ssdomains = config['use_ssdomains']
+            if use_ssdomains:
+                ssdomains = get_ssdomains(name, fdomains)
+                pae = None
+            else:
+                input_pae = config['input_pae']
+                pae = load_pae(input_pae)
+                ssdomains = None
+    else:
+        path2pdb = ''
+        use_hnetwork = False
+        pae = None
+        ssdomains = None
+    # load residue parameters
+    residues = load_parameters(cwd, dataset, cycle, initial_type)
+    # build protein dataframe
+    df = pd.DataFrame(columns=['pH', 'ionic', 'temp', 'eps_factor','fasta'], dtype=object)
+    df.loc[record] = dict(pH=pH, ionic=ionic, temp=temp, eps_factor=eps_factor, fasta=seq)
+    prot = df.loc[record]
+    print("pH:", prot.pH)
+    print("ionic:", prot.ionic)
+    print("temp:", prot.temp)
+    # LJ and YU parameters
+    lj_eps, fasta, types, MWs = genParamsLJ(residues, record, prot)
+    print("lj_eps:", lj_eps * unit.kilojoules_per_mole)
+    yukawa_eps, yukawa_kappa = genParamsDH(residues, record, prot, temp)
+
+    N = len(fasta)  # number of residues
+
+    if slab:  # overrides L from config for now
+        L, Lz, margin, Nsteps = slab_dimensions(N)
+        xy, n_chains = slab_xy(L, margin)
+    else:
+        n_chains = 1
+        Lz = L
+
+    # get input geometry
+    if slab:
+        if use_pdb:
+            raise
+        else:
+            pos = []
+            for x, y in xy:
+                pos.append([[x, y, Lz / 2 + (i - N / 2.) * .38] for i in range(N)])
+            pos = np.array(pos).reshape(n_chains * N, 3)
+    else:
+        if use_pdb:
+            print(f'Starting from pdb structure {path2pdb}')
+            pos, center_of_mass = geometry_from_pdb(cwd, dataset, name, record, cycle, replica, path2pdb, CoarseGrained=CoarseGrained, ssdomains=ssdomains)
+        else:
+            spiral = True
+            if spiral:
+                pos = xy_spiral_array(N)
+            else:
+                pos = [[L / 2, L / 2, L / 2 + (i - N / 2.) * .38] for i in range(N)]
+            pos = np.array(pos)
+
+    top = build_topology(fasta, n_chains=n_chains)
+    md.Trajectory(pos + center_of_mass, top, 0, [L, L, Lz], [90, 90, 90]).save_pdb(f'{cwd}/{dataset}/{record}/{cycle}/ini_beads.pdb', force_overwrite=True)
+    pos = pos + np.array([L / 2, L / 2, L / 2])
+    a = md.Trajectory(pos, top, 0, [L, L, Lz], [90, 90, 90])
+    a.save_pdb(f'{cwd}/{dataset}/{record}/{cycle}/top_{replica}.pdb', force_overwrite=True)
+    # build openmm system
+    system = openmm.System()
+
+    # box
+    a, b, c = build_box(L, L, Lz)
+    system.setDefaultPeriodicBoxVectors(a, b, c)
+
+    # load topology into system
+    pdb = app.pdbfile.PDBFile(f'{cwd}/{dataset}/{record}/{cycle}/top_{replica}.pdb')
+
+    # print(pdb.topology)
+
+    # particles and termini
+    system = add_particles(system, residues, prot, n_chains=n_chains)
+    dmap = euclidean(pos, pos)
+    # interactions
+    print('cutoff:', cutoff * unit.nanometer)
+    hb, yu, ah = set_interactions(system, residues, prot, lj_eps, cutoff, yukawa_kappa, yukawa_eps, N,
+                                  n_chains=n_chains, CoarseGrained=CoarseGrained, dismatrix=dmap, isIDP=isIDP,
+                                  fdomains=fdomains)
+    system.addForce(hb)
+    system.addForce(yu)
+    system.addForce(ah)
+
+    print("use_hnetwork: ", use_hnetwork)
+    # harmonic network (currently unavailable for slab sim)
+    if use_hnetwork:
+        if slab:
+            raise
+        else:
+            cs, yu, ah = set_harmonic_network(N, dmap, pae, yu, ah, ssdomains=ssdomains, k_restraint=k_restraint)
+            print(f"k_restraint used: {k_restraint}")
+            system.addForce(cs)
+
+    # use langevin integrator
+    integrator = openmm.openmm.LangevinIntegrator(temp * unit.kelvin, 0.01 / unit.picosecond, 0.01 * unit.picosecond)
+    print(integrator.getFriction(), integrator.getTemperature())
+    open(f'{cwd}/{dataset}/{record}/{cycle}/system_pro.xml', 'w').write(openmm.XmlSerializer.serialize(system))
+    if replica==0:
+        open(f'{cwd}/{dataset}/{record}/{cycle}/system_pro.xml', 'w').write(openmm.XmlSerializer.serialize(system))
+    # assemble simulation
+    if gpu:
+        # os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+        os.environ["CUDA_VISIBLE_DEVICES"] = "0"  # feasible
+        os.system("echo $CUDA_VISIBLE_DEVICES")
+        # os.system("export CUDA_VISIBLE_DEVICES=1")
+        # platform = openmm.Platform.getPlatformByName("CUDA")
+        simulation = app.simulation.Simulation(pdb.topology, system, integrator,
+                                               openmm.Platform.getPlatformByName("CUDA"),
+                                               {"DeviceIndex": f"{gpu_id}"})
+    else:
+        platform = openmm.Platform.getPlatformByName('CPU')
+        simulation = app.simulation.Simulation(pdb.topology, system, integrator, platform, dict(Threads=str(Threads)))
+
+    if Usecheckpoint and os.path.isfile(f"{cwd}/{dataset}/{record}/{cycle}/checkpoint{replica}.chk"):
+        # make sure steps need to be substracted
+        print(f'Reading check point file checkpoint{replica}.chk')
+        # load pos and vel of last frame, and chk file can be used within different nodes at least on Computerome
+        simulation.loadCheckpoint(f"{cwd}/{dataset}/{record}/{cycle}/checkpoint{replica}.chk")
+        # print(simulation.context.getState(getVelocities=True).getVelocities(asNumpy=True))
+        simulation.reporters.append(
+            app.dcdreporter.DCDReporter(f'{cwd}/{dataset}/{record}/{cycle}/{replica}_con.dcd', wfreq,
+                                        enforcePeriodicBox=False, append=False))
+    else:
+        simulation.context.setPositions(pdb.positions)
+        simulation.minimizeEnergy()
+        # state = simulation.context.getState(getPositions=True)
+        # pos2 = state.getPositions(asNumpy=True)
+        # enforcePeriodicBox=False makes sure that the saved molecules are whole;
+        # when openmm tries to keep the center of mass of molecule in the box, it will modify the x,y,z of every atoms separately;
+        # which means only if x(or y or z) of COM is out of box, it will modify x of every atom so that x of COM is in the box;
+        # Therefore, if just the x of a single atom is out of box, x might not be modified into the box;
+        # and the center of simulation box is (L/2, L/2, L/2)
+        simulation.reporters.append(app.dcdreporter.DCDReporter(f'{cwd}/{dataset}/{record}/{cycle}/{replica}.dcd', wfreq,
+                                                                enforcePeriodicBox=False, append=False))
+
+    simulation.reporters.append(
+        app.statedatareporter.StateDataReporter(f'{cwd}/{dataset}/{record}/{cycle}/statedata_{replica}.log', int(wfreq),
+                                                step=True, speed=True, elapsedTime=True, separator='\t', progress=True,
+                                                remainingTime=True, totalSteps=steps))
+    simulation.reporters.append(
+        app.checkpointreporter.CheckpointReporter(file=f"{cwd}/{dataset}/{record}/{cycle}/checkpoint{replica}.chk",
+                                                  reportInterval=wfreq))
+
+
+    starttime = time.time()  # begin timer
+    simulation.step(steps)
+    endtime = time.time()  # end timer
+    target_seconds = endtime - starttime  # total used time
+    print(
+        f"{name} total simulations used time: {target_seconds // 3600}h {(target_seconds // 60) % 60}min {np.round(target_seconds % 60, 2)}s")
+
+def centerDCD_simple(config):
+    record = config["record"]
+    cwd = config["cwd"]
+    seq = config["seq"]
+    nframes = config["nframes"]
+    dataset = config["dataset"]
+    cycle = config["cycle"]
+    replicas = config["replicas"]
+    discard_first_nframes = config["discard_first_nframes"]
+    initial_type = config["initial_type"]
+    residues = load_parameters(cwd, dataset, cycle, initial_type)
+    top = md.Topology()
+    chain = top.add_chain()
+    for resname in seq:
+        residue = top.add_residue(residues.loc[resname, 'three'], chain)
+        top.add_atom(residues.loc[resname, 'three'], element=md.element.carbon, residue=residue)
+    for i in range(len(seq) - 1):
+        top.add_bond(top.atom(i), top.atom(i + 1))
+    traj = md.load_dcd(f"{cwd}/{dataset}/{record}/{cycle}/0.dcd", top=top)[discard_first_nframes:]
+    for i in range(1, replicas):
+        t = md.load_dcd(f"{cwd}/{dataset}/{record}/{cycle}/{i}.dcd", top=top)[discard_first_nframes:]
+        traj = md.join([traj, t])
+    traj = traj.image_molecules(inplace=False, anchor_molecules=[set(traj.top.chain(0).atoms)], make_whole=True)
+    assert len(traj) == int(nframes * replicas)
+    traj.center_coordinates()
+    traj.xyz += traj.unitcell_lengths[0, 0] / 2
+    print(f'Number of frames: {traj.n_frames}')
+    traj.save_dcd(f'{cwd}/{dataset}/{record}/{cycle}/{record}.dcd')
+    traj[0].save_pdb(f'{cwd}/{dataset}/{record}/{cycle}/{record}.pdb')
+    visualize_traj(cwd, dataset, record, cycle)
 
 if __name__ == '__main__':
     pass
